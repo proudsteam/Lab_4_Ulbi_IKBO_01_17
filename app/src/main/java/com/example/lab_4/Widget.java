a3844307c8bbe09996047d2c7070385502cfee48
utimur <akva_ten@mail.ru>
2019-05-25 21:21:59 +0300

    афы

--- app/src/main/java/com/example/lab_4/Widget.java
+++ app/src/main/java/com/example/lab_4/Widget.java
@@ -70,9 +70,9 @@ public class Widget extends AppWidgetProvider {
         calendar.set(Calendar.YEAR, cal.get(Calendar.YEAR));
         calendar.set(Calendar.MONTH, cal.get(Calendar.MONTH));
         calendar.set(Calendar.DAY_OF_MONTH, cal.get(Calendar.DAY_OF_MONTH));
-        calendar.set(Calendar.HOUR_OF_DAY, 21);
-        calendar.set(Calendar.MINUTE, 17);
-        calendar.set(Calendar.SECOND, 20);
+        calendar.set(Calendar.HOUR_OF_DAY, 9);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
         calendar.set(Calendar.MILLISECOND, 0);
 
         am.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),AlarmManager.INTERVAL_DAY, pIntent1);
